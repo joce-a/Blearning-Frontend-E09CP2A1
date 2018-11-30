f96d89670c648ecb3bcb4eebd74c8545d09e642f
Joce Acuña <joce.acuna@gmail.com>
2018-11-30 11:36:49 -0300

    ADD: homework complete

--- test/app.js
+++ test/app.js
@@ -1,7 +1,45 @@
-$.ajax({
-	url: 'http://www.mindicador.cl/api',
-	method: 'GET'
-}).then(data){
-	console.log(data);
-};
+$(function(){
 
+var dolar 	= $('#dolar');
+var euro	= $('#euro');
+var uf		= $('#uf');
+var utm		= $('#utm');
+
+var url			= 'https://mindicador.cl/api'
+var queryParams = ['uf,dolar,euro,utm'];
+
+var precioDolar, precioEuro, precioUf, precioUtm;
+
+$.ajax({
+	url: url + '/' + '?' + queryParams[0] + '&' + queryParams[1] + '&' + queryParams[2] + '&' + queryParams[3],
+	method: 'GET',
+	dataType: 'json',
+})
+.done(function(data){
+	dolar.text(data.dolar.valor)
+	precioDolar = data.dolar.valor
+	euro.text(data.euro.valor)
+	precioEuro 	= data.euro.valor
+	uf.text(data.uf.valor)
+	precioUf 	= data.uf.valor
+	utm.text(data.utm.valor)
+	precioUtm 	= data.utm.valor	
+});
+
+	$('#cambio-clp').on('click', function(e){
+		e.preventDefault();
+		$('#dolar').text(($('#numero').val() / precioDolar).toFixed(2));
+		$('#euro').text(($('#numero').val() / precioEuro).toFixed(2));
+		$('#uf').text(($('#numero').val() / precioUf).toFixed(2));
+		$('#utm').text(($('#numero').val() / precioUtm).toFixed(2));
+	});
+
+	$('#cambio-usd').on('click', function(e){
+		e.preventDefault();
+		$('#clp').text(($('#numero-2').val() * precioDolar).toFixed(2));
+		//$('#hide').addAttr('hidden');
+	})
+
+
+
+}); //document.ready
